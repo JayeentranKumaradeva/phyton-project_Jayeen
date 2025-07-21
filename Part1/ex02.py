75f240557c5b4dad713aa79bdcfa6f2ce3e1525b
JayeentranKumaradeva <25086984@imail.sunway.edu.my>
2025-07-21 05:56:49 +0000

    work done for today 1

---
+++ Part1/ex02.py
@@ -0,0 +1,2 @@
+name = "Jia"
+print (name)
---
+++ Part1/ex08.py
@@ -0,0 +1,10 @@
+# Current speed
+speed = 50
+print(speed)
+
+# TODO:
+# If the speed is more than 60,
+# print "Over the speed limit!"
+
+if speed > 60:
+    print("Over the speed limit")
